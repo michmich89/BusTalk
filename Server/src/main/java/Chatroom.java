763b16cd4dcd94c61f1efea8345cf770d85d1f3e
krkn0001 <kristoffer_knutsson@hotmail.com>
2015-09-29 13:06:23 +0200

    Chatroom and prelims
    Chatroom and a shell for main server class...

---
+++ Server/src/main/java/Chatroom.java
@@ -0,0 +1,35 @@
+import javax.websocket.Session;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kristoffer on 2015-09-29.
+ */
+public class Chatroom {
+
+    private final int idNbr;
+    private final List<Session> chatroomUsers;
+
+    public Chatroom(int idNbr){
+        this.idNbr = idNbr;
+        chatroomUsers = new ArrayList<Session>();
+    }
+
+    public void subscribeToRoom(Session userSession){
+        chatroomUsers.add(userSession);
+    }
+
+    public void unsubscribeToRoom(Session userSession){
+        chatroomUsers.remove(userSession);
+    }
+
+    public int getIdNbr(){
+        return this.idNbr;
+    }
+
+    public List<Session> getChatroomUsers(){
+        return new ArrayList<Session>(chatroomUsers);
+    }
+
+
+}
--- Server/src/main/java/ServerEndpoint.java
+++ Server/src/main/java/ServerEndpoint.java
@@ -1,5 +1,19 @@
 /**
  * Created by Kristoffer on 2015-09-29.
  */
+
+import javax.websocket.*;
+
 public class ServerEndpoint {
+
+    
+
+    public ServerEndpoint(){
+
+    }
+
+
+
+
+
 }
