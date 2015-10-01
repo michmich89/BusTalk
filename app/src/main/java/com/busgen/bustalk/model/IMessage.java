06db3bfec67ead207891197989275f73024dca30
Johan Iversen <johan.iversen@gmail.com>
2015-10-01 17:17:17 +0200

    Added methods to IMessage and created Message class

--- app/src/main/java/com/busgen/bustalk/model/IMessage.java
+++ app/src/main/java/com/busgen/bustalk/model/IMessage.java
@@ -1,8 +1,15 @@
 package com.busgen.bustalk.model;
 
 /**
- * Message sent by an user in a chatroom.
+ * Message sent by a user in a chatroom.
  */
 public interface IMessage {
+
+    String getType();
+    String getMessage();
+    int getChatID();
+    IUser getUser();
+    String toString();
+    void parse();
 
 }
