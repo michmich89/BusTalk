2c8c5d71a1b6d26290fe81888c3af5328e8fe87c
Rektorn <Nalexdotk@gmail.com>
2015-10-02 11:37:54 +0200

    Added Json encoder and decoder classes and ServerCommunicator, needs implementation

---
+++ app/src/main/java/com/busgen/bustalk/service/JsonDecoder.java
@@ -0,0 +1,8 @@
+package com.busgen.bustalk.service;
+
+/**
+ * Created by Alexander Kloutschek on 2015-10-02.
+ */
+public class JsonDecoder {
+
+}
---
+++ app/src/main/java/com/busgen/bustalk/service/JsonEncoder.java
@@ -0,0 +1,7 @@
+package com.busgen.bustalk.service;
+
+/**
+ * Created by Alexander Kloutschek on 2015-10-02.
+ */
+public class JsonEncoder {
+}
