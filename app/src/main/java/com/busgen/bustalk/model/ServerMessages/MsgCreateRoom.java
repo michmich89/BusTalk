1f9fb45cf6f0c6457dc8f51cdd56fb738b6816a3
Rektorn <Nalexdotk@gmail.com>
2015-10-05 20:19:08 +0200

    Added appropriate implementation to servermessage and JSONConverter

--- app/src/main/java/com/busgen/bustalk/model/ServerMessages/MsgCreateRoom.java
+++ app/src/main/java/com/busgen/bustalk/model/ServerMessages/MsgCreateRoom.java
@@ -6,9 +6,11 @@ import com.busgen.bustalk.model.IServerMessage;
  * Created by Johan on 2015-10-05.
  */
 public class MsgCreateRoom implements IServerMessage {
+    int chatID;
 
-
-    public MsgCreateRoom(){
-
+    public MsgCreateRoom(int chatID){
+        this.chatID = chatID;
     }
+
+    public int getChatID(){ return chatID;}
 }
