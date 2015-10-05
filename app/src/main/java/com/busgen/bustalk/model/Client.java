ae2b0704d365aff8f023ade11039fb98585043cc
Johan Iversen <johan.iversen@gmail.com>
2015-10-05 17:57:37 +0200

    started implementing Client class

--- app/src/main/java/com/busgen/bustalk/model/Client.java
+++ app/src/main/java/com/busgen/bustalk/model/Client.java
@@ -1,8 +1,43 @@
 package com.busgen.bustalk.model;
 
+import java.util.List;
+
 /**
  * Created by Johan on 2015-10-05.
  */
 public class Client implements IClient{
 
+    private IUser user;
+    private List<IChatroom> chatrooms;
+    private Client instance;
+
+    private Client (IUser user){
+
+    }
+
+    public Client (IUser user){
+        if (this.instance.equals(null)){
+            return new 
+        }
+    }
+
+    @Override
+    public IUser getUser() {
+        return null;
+    }
+
+    @Override
+    public IChatroom[] getChatrooms() {
+        return chatrooms;
+    }
+
+    @Override
+    public List<IServerMessage> recieveMessages() {
+        return null;
+    }
+
+    @Override
+    public void sendMessage(IServerMessage serverMessage) {
+
+    }
 }
