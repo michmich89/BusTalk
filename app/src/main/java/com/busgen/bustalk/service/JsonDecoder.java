92571943ded3a768558738b02cb6930aa6de64cf
Alexander Kloutschek <Nalex.k@hotmail.com>
2015-10-05 17:13:55 +0200

    Removed JsonEncoder/Decoder and replaced with JSONConverter. Needs further implementation

--- app/src/main/java/com/busgen/bustalk/service/JsonDecoder.java
+++
@@ -1,8 +0,0 @@
-package com.busgen.bustalk.service;
-
-/**
- * Created by Alexander Kloutschek on 2015-10-02.
- */
-public class JsonDecoder {
-
-}
--- app/src/main/java/com/busgen/bustalk/service/JsonEncoder.java
+++
@@ -1,7 +0,0 @@
-package com.busgen.bustalk.service;
-
-/**
- * Created by Alexander Kloutschek on 2015-10-02.
- */
-public class JsonEncoder {
-}
