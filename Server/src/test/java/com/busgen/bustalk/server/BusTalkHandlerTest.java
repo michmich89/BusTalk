a047ffe4cce6b48d78c4f0d432b4e35d05ba1be5
krkn0001 <kristoffer_knutsson@hotmail.com>
2015-10-15 11:57:17 +0200

    JUnit testing prep.

--- Server/src/test/java/com/busgen/bustalk/server/BusTalkHandlerTest.java
+++ Server/src/test/java/com/busgen/bustalk/server/BusTalkHandlerTest.java
@@ -1,5 +1,17 @@
 package com.busgen.bustalk.server;
 
+import com.busgen.bustalk.server.chatroom.ChatroomHandler;
+import com.busgen.bustalk.server.user.User;
+import com.busgen.bustalk.server.user.UserHandler;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +19,162 @@ import static org.junit.Assert.*;
  */
 public class BusTalkHandlerTest {
 
+    private ChatroomHandler chatroomHandler;
+    private UserHandler userHandler;
+    private User user;
+
+    public BusTalkHandlerTest(){
+        chatroomHandler = ChatroomHandler.getInstance();
+        userHandler = UserHandler.getInstance();
+    }
+
+
+    private Session createNewSession() {
+        return new Session() {
+            @Override
+            public WebSocketContainer getContainer() {
+                return null;
+            }
+
+            @Override
+            public void addMessageHandler(MessageHandler handler) throws IllegalStateException {
+
+            }
+
+            @Override
+            public <T> void addMessageHandler(Class<T> clazz, MessageHandler.Whole<T> handler) {
+
+            }
+
+            @Override
+            public <T> void addMessageHandler(Class<T> clazz, MessageHandler.Partial<T> handler) {
+
+            }
+
+            @Override
+            public Set<MessageHandler> getMessageHandlers() {
+                return null;
+            }
+
+            @Override
+            public void removeMessageHandler(MessageHandler handler) {
+
+            }
+
+            @Override
+            public String getProtocolVersion() {
+                return null;
+            }
+
+            @Override
+            public String getNegotiatedSubprotocol() {
+                return null;
+            }
+
+            @Override
+            public List<Extension> getNegotiatedExtensions() {
+                return null;
+            }
+
+            @Override
+            public boolean isSecure() {
+                return false;
+            }
+
+            @Override
+            public boolean isOpen() {
+                return false;
+            }
+
+            @Override
+            public long getMaxIdleTimeout() {
+                return 0;
+            }
+
+            @Override
+            public void setMaxIdleTimeout(long milliseconds) {
+
+            }
+
+            @Override
+            public void setMaxBinaryMessageBufferSize(int length) {
+
+            }
+
+            @Override
+            public int getMaxBinaryMessageBufferSize() {
+                return 0;
+            }
+
+            @Override
+            public void setMaxTextMessageBufferSize(int length) {
+
+            }
+
+            @Override
+            public int getMaxTextMessageBufferSize() {
+                return 0;
+            }
+
+            @Override
+            public RemoteEndpoint.Async getAsyncRemote() {
+                return null;
+            }
+
+            @Override
+            public RemoteEndpoint.Basic getBasicRemote() {
+                return null;
+            }
+
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public void close() throws IOException {
+
+            }
+
+            @Override
+            public void close(CloseReason closeReason) throws IOException {
+
+            }
+
+            @Override
+            public URI getRequestURI() {
+                return null;
+            }
+
+            @Override
+            public Map<String, List<String>> getRequestParameterMap() {
+                return null;
+            }
+
+            @Override
+            public String getQueryString() {
+                return null;
+            }
+
+            @Override
+            public Map<String, String> getPathParameters() {
+                return null;
+            }
+
+            @Override
+            public Map<String, Object> getUserProperties() {
+                return null;
+            }
+
+            @Override
+            public Principal getUserPrincipal() {
+                return null;
+            }
+
+            @Override
+            public Set<Session> getOpenSessions() {
+                return null;
+            }
+        };
+    }
 }
