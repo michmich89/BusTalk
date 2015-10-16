fc6120e52da35ac3b47eeb5b5ed63ba99523e8cf
Alexander Kloutschek <Nalex.k@hotmail.com>
2015-10-16 16:05:24 +0200

    Added new message objects for some new user stories

---
+++ app/src/main/java/com/busgen/bustalk/model/ServerMessages/MsgUsersInChat.java
@@ -0,0 +1,32 @@
+package com.busgen.bustalk.model.ServerMessages;
+
+import com.busgen.bustalk.model.IUser;
+import com.busgen.bustalk.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nalex on 16/10/2015.
+ */
+public class MsgUsersInChat {
+    List<IUser> userList;
+    int chatId;
+
+    public MsgUsersInChat(int chatId){
+        this.chatId = chatId;
+        userList = new ArrayList<IUser>();
+    }
+
+    public List<IUser> getUserList(){
+        return userList;
+    }
+
+    public void addUserToList(IUser user){
+        userList.add(user);
+    }
+
+    public int getChatID(){
+        return chatId;
+    }
+}
