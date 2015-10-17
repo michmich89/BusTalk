cf5ee8795836f13f1ff57068956b429ee672e53a
Johan Iversen <johan.iversen@gmail.com>
2015-10-17 16:38:40 +0200

    Created Main Service and made preparations for binding activities to it

---
+++ app/src/main/java/com/busgen/bustalk/service/MainService.java
@@ -0,0 +1,60 @@
+package com.busgen.bustalk.service;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.busgen.bustalk.ServerCommunicator;
+import com.busgen.bustalk.model.Client;
+import com.busgen.bustalk.model.IClient;
+import com.busgen.bustalk.model.IEventBusListener;
+
+public class MainService extends Service {
+
+    private static MainService instance;
+
+    private final IBinder binder = new MainBinder();
+    private Client client;
+    private ServerCommunicator serverCommunicator;
+    private EventBus eventBus;
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return Service.START_REDELIVER_INTENT;
+    }
+
+    @Override
+    public void onCreate(){
+
+        client = Client.getInstance();
+        serverCommunicator = new ServerCommunicator();
+        eventBus = EventBus.getInstance();
+
+        eventBus.register(client);
+        eventBus.register(serverCommunicator);
+
+    }
+
+
+    @Override
+    public void onDestroy(){
+
+    }
+
+    public class MainBinder extends Binder {
+        public EventBus getEventBus() {
+            return MainService.this.eventBus;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+}
