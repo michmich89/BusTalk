232ffb7c39f7483594321dae22f37f19e998b04b
Rektorn <Nalexdotk@gmail.com>
2015-10-18 16:59:47 +0200

    Added appropriate interfaces to the two new platform messages

--- app/src/main/java/com/busgen/bustalk/model/ServerMessages/MsgPlatformData.java
+++ app/src/main/java/com/busgen/bustalk/model/ServerMessages/MsgPlatformData.java
@@ -1,9 +1,11 @@
 package com.busgen.bustalk.model.ServerMessages;
 
+import com.busgen.bustalk.model.IServerMessage;
+
 /**
  * Created by Alexander Kloutschek on 2015-10-18.
  */
-public class MsgPlatformData {
+public class MsgPlatformData implements IServerMessage{
     private String data;
     private String dataType;
 
--- app/src/main/java/com/busgen/bustalk/model/ServerMessages/MsgPlatformDataRequest.java
+++ app/src/main/java/com/busgen/bustalk/model/ServerMessages/MsgPlatformDataRequest.java
@@ -1,9 +1,11 @@
 package com.busgen.bustalk.model.ServerMessages;
 
+import com.busgen.bustalk.model.IServerMessage;
+
 /**
  * Created by Alexander Kloutschek on 2015-10-18.
  */
-public class MsgPlatformDataRequest {
+public class MsgPlatformDataRequest implements IServerMessage{
     private String bussID;
     //här skulle man kunna beskriva vilken sorts data det är man vill skicka kanske?
     private String dataType;
