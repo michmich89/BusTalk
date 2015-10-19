370cccaaf52231f74215790926d75ae8d06d6d19
Wassbjer <daniel.wassbjer@gmail.com>
2015-10-19 16:53:57 +0200

    Add more logic to ConnectionsHandler

---
+++ app/src/main/java/com/busgen/bustalk/ConnectionsHandler.java
@@ -0,0 +1,67 @@
+package com.busgen.bustalk;
+
+import com.busgen.bustalk.events.Event;
+import com.busgen.bustalk.events.ToActivityEvent;
+import com.busgen.bustalk.events.ToClientEvent;
+import com.busgen.bustalk.model.IEventBusListener;
+import com.busgen.bustalk.model.ServerMessages.MsgConnectionLost;
+import com.busgen.bustalk.model.ServerMessages.MsgPlatformData;
+import com.busgen.bustalk.service.EventBus;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by nalex on 19/10/2015.
+ */
+public class ConnectionsHandler implements IEventBusListener{
+    private ServerCommunicator serverCom;
+    private PlatformCommunicator platformCom;
+    private WifiController wifiController;
+    private boolean connectionStatus;
+    private boolean isTest;
+    private EventBus eventBus;
+
+    private Timer timer;
+    private TimerTask timerTask;
+
+    public ConnectionsHandler(){
+        super();
+        serverCom = new ServerCommunicator("ws://sandra.kottnet.net:8080/BusTalkServer/chat");
+        platformCom = new PlatformCommunicator();
+        wifiController = new WifiController();
+        eventBus = EventBus.getInstance();
+        eventBus.register(this);
+
+        timer = new Timer("wifiCheck");
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (!getConnectionStatus()) {
+                    sendConnectionStatus();
+                    //String nextStop = platformCom.getNextStopData(wifiController.getWifiName());
+                    //sendNextStopData(nextStop);
+                }
+            }
+
+            private boolean getConnectionStatus(){
+                return (isTest || wifiController.isConnected()) && serverCom.isConnected();
+            }
+
+            private void sendConnectionStatus(){
+                eventBus.postEvent(new ToClientEvent(new MsgConnectionLost()));
+            }
+
+            private void sendNextStopData(String nextStop) {
+                eventBus.postEvent(new ToActivityEvent(new MsgPlatformData(nextStop)));
+            }
+        };
+
+        timer.schedule(timerTask, 15000);
+    }
+
+    @Override
+    public void onEvent(Event e){
+
+    }
+}
---
+++ app/src/main/java/com/busgen/bustalk/model/ServerMessages/MsgConnectionLost.java
@@ -0,0 +1,6 @@
+package com.busgen.bustalk.model.ServerMessages;
+
+import com.busgen.bustalk.model.IServerMessage;
+
+public class MsgConnectionLost implements IServerMessage {
+}
