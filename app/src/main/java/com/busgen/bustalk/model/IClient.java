cda8a98b36efaac67619494bec73f0c32926c2a3
Johan Iversen <johan.iversen@gmail.com>
2015-10-30 14:11:36 +0100

    Abstracted much on Client's onEvent code to methods

--- app/src/main/java/com/busgen/bustalk/model/IClient.java
+++ app/src/main/java/com/busgen/bustalk/model/IClient.java
@@ -4,6 +4,7 @@ import com.busgen.bustalk.model.ServerMessages.MsgChatMessage;
 import com.busgen.bustalk.service.EventBus;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Interface representing a a Client connected to the Chat server.
@@ -23,7 +24,14 @@ public interface IClient {
     Collection<IServerMessage> recieveMessages();
     void sendMessage(IServerMessage serverMessage);
 
+    void setUsers(int chatId, List<IUser> userList);
+    void addUser(int chatId, IUser user);
+    void removeUser(int chatId, IUser user);
+    void requestUsersFromServer(int chatId);
+    void setGroupId(String groupId);
+    String getGroupId();
 
     void joinRoom (IChatroom chatroom);
     void leaveRoom (IChatroom chatroom);
+    void leaveRoom(int chatId);
 }
